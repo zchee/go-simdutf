c694d18907df9eef0ea1144520e7d0c75db1b939
Koichi Shiraishi <zchee.io@gmail.com>
2023-01-09 01:24:38 +0900

    simdutf: add initial simdutf.go file
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ simdutf.go
@@ -0,0 +1,7 @@
+// Copyright 2023 The go-simdutf Authors
+// SPDX-License-Identifier: BSD-3-Clause
+
+// Package simdutf provides a Go port of https://github.com/simdutf/simdutf.
+//
+// Unicode routines (UTF8, UTF16, UTF32): billions of characters per second using SSE2, AVX2, NEON and AVX-512.
+package simdutf
